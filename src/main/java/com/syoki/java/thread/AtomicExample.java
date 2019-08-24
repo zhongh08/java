49965cd21cd9c94bb96dc4a9b4d783f639aea1d1
zhongh08 <zhongh08@163.com>
2019-08-24 13:20:38 +0800

    AtomicExample

---
+++ src/main/java/com/syoki/java/thread/AtomicExample.java
@@ -0,0 +1,36 @@
+package com.syoki.java.thread;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicExample {
+
+    private AtomicInteger cnt = new AtomicInteger();
+
+    public void add() {
+        cnt.incrementAndGet();
+    }
+
+    public int get() {
+        return cnt.get();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        final int threadSize = 1000;
+        AtomicExample example = new AtomicExample();
+        final CountDownLatch countDownLatch = new CountDownLatch(threadSize);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i<threadSize; i++) {
+            executorService.execute(()->{
+                example.add();
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        System.out.println(example.get());
+    }
+
+}
